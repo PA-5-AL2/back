3c856fb92264c6d79d2193276518b524c64bc4ed
samiraseddar <seddar20191@gmail.com>
2025-04-20 15:11:41 +0200

    add spring security + add jwt + add mysql ( bdd active )

---
+++ src/main/java/esgi/easisell/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package esgi.easisell.service;
+
+import esgi.easisell.entity.User;
+import esgi.easisell.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user= userRepository.findByUsername(username);
+        if(user==null){
+            throw new UsernameNotFoundException("user not found "+username);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),Collections.singletonList(new SimpleGrantedAuthority(user.getRole())));
+    }
+}
